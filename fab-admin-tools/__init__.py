b8b2cb63802fe77340cddbcc8e985fa39b3440ee
daniel liu <daniel_001@126.com>
2016-05-04 11:48:56 +0800

    add nginx tools

---
+++ fab-admin-tools/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#__author__ = 'daniel'
+#__date__=
+#__email__='daniel_001@126.com'
+
+
+
+
+if __name__ == '__main__':
+    pass
---
+++ fab-admin-tools/nginx-tools.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#__author__ = 'daniel'
+#__date__=
+#__email__='daniel_001@126.com'
+
+from fabric.api import *
+
+env.key_filename = '~/.ssh/id_rsa'
+env.user='root'
+
+
+if __name__ == '__main__':
+    pass
